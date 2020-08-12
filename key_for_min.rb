7d3c4bc234c0b5788f4e682d0268eb62913a8a4c
Adam Adolfo <adamadolfo8@gmail.com>
2020-08-12 13:56:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   lowest_value = 1000
   name_hash.each do |key, value|
   if value < lowest_value
-    value = lowest_value
+    key = lowest_value
   end
 end
   key
